0c3e3d2c28be4c3a5d9b27ae8cd3a93938220be6
minbo2002 <minbo2002@naver.com>
2022-10-18 14:37:14 +0900

    이미지 쇼핑몰 프로젝트 환경 설정 및 DB 연동

---
+++ src/test/java/org/shop/ShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
